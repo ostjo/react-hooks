2f739a03f5dd8f65e34c62c0a6a99e80e01e5ddc
ostjo <johanna@finmid.com>
2023-07-14 16:43:02 +0200

    add extra credit 02

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,16 +2,11 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
 function Board() {
   const emptyBoard = Array(9).fill(null)
-  const [squares, setSquares] = React.useState(
-    () => JSON.parse(window.localStorage.getItem('squares')) || emptyBoard,
-  )
-
-  React.useEffect(() => {
-    window.localStorage.setItem('squares', JSON.stringify(squares))
-  }, [squares])
+  const [squares, setSquares] = useLocalStorageState('squares', emptyBoard)
 
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
